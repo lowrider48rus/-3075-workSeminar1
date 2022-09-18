54921dd82d2bfffd465cc4ba9dbae9b2f60c874f
lowrider48rus <lowrider48rus@gmail.com>
2022-09-18 15:38:27 +0300

    убрал лишнее из кода

--- zadacha8/Program.cs
+++ zadacha8/Program.cs
@@ -1,5 +1,4 @@
 ﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
-//Console.WriteLine($"Number {num} IS NOT even number");
 
 Console.WriteLine("Type number: ");
 int num = int.Parse(Console.ReadLine());
