834b9833ac3eba4b864cb114027b169a58b75ef3
lowrider48rus <lowrider48rus@gmail.com>
2022-09-18 15:02:13 +0300

    задача 6 решена

---
+++ zadacha6/Program.cs
@@ -0,0 +1,12 @@
+﻿// Напишите программу, которая на вход принимает число и выдаёт, является ли число чётным (делится ли оно на два без остатка).
+
+Console.WriteLine("Type number: ");
+int num = int.Parse(Console.ReadLine());
+int resid = num % 2;
+
+if (resid == 1){
+    Console.WriteLine($"Number {num} IS NOT even number");
+}
+else {
+    Console.WriteLine($"Number {num} IS even number");
+}
