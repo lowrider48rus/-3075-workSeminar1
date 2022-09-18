25077895a50df79e567ac1f39fcbef17b185e9c0
lowrider48rus <lowrider48rus@gmail.com>
2022-09-18 15:35:02 +0300

    задача 8 решена

---
+++ zadacha8/Program.cs
@@ -0,0 +1,19 @@
+﻿// Напишите программу, которая на вход принимает число (N), а на выходе показывает все чётные числа от 1 до N.
+//Console.WriteLine($"Number {num} IS NOT even number");
+
+Console.WriteLine("Type number: ");
+int num = int.Parse(Console.ReadLine());
+int i = 1;
+while (i < (num+1))
+{
+    if (i % 2 == 0)
+    {
+        Console.Write(i+" ");
+        i++;
+    }
+    else
+    {
+        i++;
+    }
+}
+
